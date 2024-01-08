7a3e01986d7055ffe1f71d1b2768babdc573a13b
Yernur Satybaldiyev <yernur@Yernurs-MacBook-Pro.local>
2024-01-08 11:17:35 +0600

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from blog import views
+
+
+
+urlpatterns = [
+    path("", views.index, name = "home"),
+    path('about/', views.about, name = "about"),
+]
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+   return render(request, "blog/index.html", context={"site": "Stepik.org"})
+
+def about(request):
+    return render(request, "blog/about.html", context={"site": "Stepik.org"})
+
+
+
+
